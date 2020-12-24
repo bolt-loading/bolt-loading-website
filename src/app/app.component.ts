1354c6ee044ea9c11cc4af9dc9417cbb0b036c83
yasgo <yasinburakkalkan@gmail.com>
2020-12-24 23:19:38 +0300

    Angular init.

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-home></app-home>
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'bolt-loading-website';
+}
