f91b40916d70a101c08bea050a6b10af7973f444
yasgo <yasinburakkalkan@gmail.com>
2020-12-28 22:57:51 +0300

    Usage page development.

--- src/app/usage/usage.component.ts
+++ src/app/usage/usage.component.ts
@@ -4,13 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-usage',
   template: `
     <app-detail>
-      <h2>Usage</h2>
+      <article class="detail-item">
+        <h4>Import</h4>
+        <app-copy-command [code]="importCode"></app-copy-command>
+      </article>
+      <article class="detail-item">
+        <h4>Usage</h4>
+        <app-copy-command [code]="usageCode"></app-copy-command>
+      </article>
     </app-detail>
   `,
   styles: [
   ]
 })
 export class UsageComponent implements OnInit {
+
+  importCode: string = "import BoltLoading, { Loadings } from 'bolt-loading';";
+
+  usageCode: string = "<BoltLoading isShow={true} />";
 
   constructor() { }
 
