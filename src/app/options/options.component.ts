ff47ddbeeb6dc1273cebbc0ab79eef4ccf7de842
yasgo <yasinburakkalkan@gmail.com>
2021-01-05 23:06:34 +0300

    Options development.

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
         <h4>isShow</h4>
         <p class="feature"><strong>Type:</strong> Boolean</p>
         <p class="feature"><strong>Default:</strong> false</p>
-        <p>Form button is disabled. If success fill form, form button is enabled.</p>
+        <p>You can control visibility.</p>
       </article>
       <article class="detail-item">
         <h4>loadingAnimation</h4>
         <p class="feature"><strong>Type:</strong> Loadings</p>
         <p class="feature"><strong>Default:</strong> Loading.AppleLoading</p>
-        <p>Form button is disabled. If success fill form, form button is enabled.</p>
+        <p>You can select loading animation. If you not select animation, default selected apple animation loading.</p>
       </article>
     </app-detail>
   `,
