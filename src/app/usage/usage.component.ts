25007ab1c4b2a7fcae16587a35fbba47236fc5d5
yasgo <yasinburakkalkan@gmail.com>
2021-01-05 23:03:12 +0300

    Usage development.

--- src/app/usage/usage.component.ts
+++ src/app/usage/usage.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
     <app-detail>
       <article class="detail-item">
         <h4>Import</h4>
+        <p class="mb-4">Firstly import bolt-loading library.</p>
         <app-copy-command [code]="importCode"></app-copy-command>
       </article>
       <article class="detail-item">
         <h4>Usage</h4>
+        <p class="mb-4">In the simplest form, you can use it this way.</p>
         <app-copy-command [code]="usageCode"></app-copy-command>
       </article>
     </app-detail>
