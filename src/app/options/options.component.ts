64f555e4c2aea02dca6c55eaa37c401187644e1b
yasgo <yasinburakkalkan@gmail.com>
2021-01-11 21:47:03 +0300

    hasContainer feature development.

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -16,6 +16,12 @@ import { Component, OnInit } from '@angular/core';
         <p class="feature"><strong>Default:</strong> Loading.AppleLoading</p>
         <p>You can select loading animation. If you not select animation, default selected apple animation loading.</p>
       </article>
+      <article class="detail-item">
+        <h4>hasContainer</h4>
+        <p class="feature"><strong>Type:</strong> Boolean</p>
+        <p class="feature"><strong>Default:</strong> false</p>
+        <p>You can change this feature if you want animation inside container.</p>
+      </article>
     </app-detail>
   `,
   styles: [
